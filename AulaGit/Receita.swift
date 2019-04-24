9d4005dd2303e2283488926f01990ac1b08de60a
Jéssica Yumi Tanimoto <tanimoto.yumi@gmail.com>
2019-04-24 14:23:05 -0300

    Adiciona o receitas

---
+++ AulaGit/Receita.swift
@@ -0,0 +1,30 @@
+//
+//  Receita.swift
+//  AulaGit
+//
+//  Created by Jéssica Yumi Tanimoto on 24/04/19.
+//  Copyright © 2019 CRASHCOURSE. All rights reserved.
+//
+
+import UIKit
+
+class Receita: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
